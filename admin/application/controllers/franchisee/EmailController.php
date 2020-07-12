61bbe94f07d47271c28c499c3ef7c9e744b19784
Doraas78 <dorasaadan@gmail.com>
2020-07-12 14:13:26 +0200

    modify newsletter

--- admin/application/controllers/franchisee/EmailController.php
+++ admin/application/controllers/franchisee/EmailController.php
@@ -10,21 +10,20 @@ class EmailController extends Controller
         $customerModel = new CustomerModel();
         $emails = $customerModel->getAllCustomersNewslettersAggreed();
 
-        var_dump($emails);
+
 
         if($emails !== null || !empty($emails) )
         {
-            $emails = implode(",", $emails);
-
-            var_dump($emails);
-
-            /*$recipient = $emails;
             $subject = $_POST['subject'];
             $content = $_POST['message'];
             $mailheader = "From: Driv'N Cook Trucks " . $_SESSION['name'] . " <no-reply@drivncooktrucks.com> \r\n";
 
-            mail($recipient, $subject, $content, $mailheader) or die("Error!");*/
-            //echo json_encode(1);
+            foreach ($emails as $email){
+                mail($email['email'], $subject, $content, $mailheader);
+
+            }
+
+            echo json_encode(1);
 
         }else{
             echo json_encode(0);
